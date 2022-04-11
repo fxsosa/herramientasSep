2f26da05d1022f497368a13f84b74078c59892f2
Matias Sosa <fuxesosa@gmail.com>
2022-04-11 10:56:14 -0400

    bkp.sh: verificacion de condicion path

--- bkp.sh
+++ bkp.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
 verificarPath(){
 	if [[ ! -f $1 && ! -d $1 ]]; then
-	    echo "$1 -- no es un archivo o directorio valido"
+		echo "$1 -- no es un archivo o directorio valido"
+		return 0
+	else
+		return 1
 	fi
 }
 leerPath(){
 	while read path
 	do
-		verificarPath $path
+		if [ -z $path ]; then
+			break
+		else
+			verificarPath $path
+			if [ "$?" -eq 0 ]; then
+				echo "condicion no verificada"
+			else
+				echo "condicion verificada"
+			fi
+
+		fi
 	done < "${1:-/dev/stdin}"
+	echo fin
 }
 leerPath
