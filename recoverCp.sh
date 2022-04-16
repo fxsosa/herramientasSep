7c049c3d588d7a356d104408ae724e946b53c6a0
Matias Sosa <fuxesosa@gmail.com>
2022-04-16 14:52:19 -0400

    recoverCp:eliminado, solo se utilizara tar

--- recoverCp.sh
+++
@@ -1,9 +0,0 @@
-read -p "Ingrese el nombre del pase, ex:OC3213, SO2345:" NOMBRE_PASE
-read -p "Ingrese el PATH del directorio de los backup:" PATH_BACKUP
-PATH_BACKUP_ACTUAL="${PATH_BACKUP}/${NOMBRE_PASE}/"
-PATH_BACKUP_ACTUAL_RECOVER="${PATH_BACKUP}/${NOMBRE_PASE}/recoverPath"
-# TODO corroborar si existe el path
-while IFS=: read -r -d '\n' SOURCE DESTINATION; do
-	sudo cp -a "${SOURCE} "${DESTINATION}"
-done<$PATH_BACKUP_ACTUAL_RECOVER
-
